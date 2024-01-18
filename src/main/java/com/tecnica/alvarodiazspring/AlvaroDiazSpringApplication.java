d57e60273bb05e000f3eabcf50fe19889cfd20a1
Alvaroodiiaz <alvarodiazgonzalez10@gmail.com>
2024-01-18 17:49:25 +0100

    Anotaciones

--- src/main/java/com/tecnica/alvarodiazspring/AlvaroDiazSpringApplication.java
+++ src/main/java/com/tecnica/alvarodiazspring/AlvaroDiazSpringApplication.java
@@ -2,8 +2,14 @@ package com.tecnica.alvarodiazspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.tecnica.alvarodiazspring"})
+@EnableJpaRepositories(basePackages = {"com.tecnica.alvarodiazspring.repository"})
+@EntityScan(basePackages = {"com.tecnica.alvarodiazspring.model"})
 public class AlvaroDiazSpringApplication {
 
 	public static void main(String[] args) {
